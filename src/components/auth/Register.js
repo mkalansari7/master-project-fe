7ed7d62ea6dea818c1dc63d9ed050c76d995e720
mkalansari7 <mk.alansari7@gmail.com>
2022-04-18 05:22:25 +0300

    add register page need style

---
+++ src/components/auth/Register.js
@@ -0,0 +1,59 @@
+import React, { useState } from "react";
+import { Button, Form, FormControl } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
+import authStore from "../../stores/authstore";
+
+const Register = () => {
+  const navigate = useNavigate();
+
+  const [user, setUser] = useState(null);
+
+  const handleChange = (event) =>
+    setUser({ ...user, [event.target.name]: event.target.value });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    authStore.signup(user);
+    navigate("/", { replace: true });
+  };
+  return (
+    <div
+      style={{
+        width: "50%",
+        margin: "auto",
+      }}
+    >
+      <Form>
+        <h1>Register</h1>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Username</Form.Label>
+          <FormControl
+            placeholder="Your username"
+            name="username"
+            type="text"
+            onChange={handleChange}
+          />
+          <Form.Text className="text-muted"></Form.Text>
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <FormControl
+            name="password"
+            type="password"
+            onChange={handleChange}
+            placeholder="Password"
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicCheckbox">
+          <Form.Check type="checkbox" label="Check me out" />
+        </Form.Group>
+        <Button variant="primary" type="submit" onClick={handleSubmit}>
+          Submit
+        </Button>
+      </Form>
+    </div>
+  );
+};
+
+export default Register;
