80c189dc0f51bff0a4a85ac51b2d043e5e693efb
mkalansari7 <mk.alansari7@gmail.com>
2022-04-18 05:21:10 +0300

    add authStore work with bachend

---
+++ src/stores/authstore.js
@@ -0,0 +1,60 @@
+import { makeAutoObservable } from "mobx";
+import { instance } from "./instance";
+
+class AuthStore {
+  user = null;
+  constructor() {
+    makeAutoObservable(this);
+  }
+
+  setUser = (token) => {
+    instance.defaults.headers.common.Authorization = `Bearer ${token}`;
+    localStorage.setItem("token", token);
+  };
+
+  signout = () => {
+    localStorage.removeItem("token");
+    this.user = null;
+  };
+
+  signup = async (userData) => {
+    try {
+      console.log(userData);
+      await instance.post("/api/users/", userData);
+      await this.signin(userData);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  signin = async (userData) => {
+    try {
+      const res = await instance.post("/api/jwt/create/", userData);
+      this.setUser(res.data.access);
+      console.log({ res });
+      const res2 = await instance.get("/api/users/me/");
+      this.user = res2.data;
+      console.log({ res2 });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  checkForToken = async () => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      try {
+        this.setUser(token);
+        const res2 = await instance.get("/api/users/me/");
+        this.user = res2.data;
+      } catch (error) {
+        this.signout();
+      }
+    } else this.signout();
+  };
+}
+
+const authStore = new AuthStore();
+authStore.checkForToken();
+
+export default authStore;
