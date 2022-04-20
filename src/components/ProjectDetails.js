f5a9b39d87bad661da5085c045b9fae7669da8ae
mkalansari7 <mk.alansari7@gmail.com>
2022-04-20 07:38:27 +0300

    list teams

--- src/components/ProjectDetails.js
+++ src/components/ProjectDetails.js
@@ -1,11 +1,16 @@
 import React from "react";
 
 const ProjectDetails = ({ project, semester }) => {
+  const teams = project.team.map((tem) => tem.name + ", ");
+  console.log(teams);
+
   return (
     <div>
       {semester.id === project.semester && (
         <div className="myCard2">
           <h5>{project.name}</h5>
+          <h5 style={{ padding: "0 5px" }}>-</h5>
+          <h6>{teams}</h6>
         </div>
       )}
     </div>
